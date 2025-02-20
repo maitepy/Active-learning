74aaed39b4cb5f49886db5853ada4d37bfe0016a
maitepy <maite@estudiodesoluciones.com>
2025-02-20 18:56:24 +0100

    Add files via upload

---
+++ 01_variables_exersices.py
@@ -0,0 +1,59 @@
+# variables 1
+name = "Maite"
+age = 43
+height = 168
+print(name)
+print(age)
+print(height)
+
+#variables 2
+str(age)
+print(type(str(age)))
+age_str = str(age)
+print(type(age_str))
+print(age_str)
+age + 1
+print("may name is Maite I am" + " " + age_str)
+print("my name is", name, "I am", age)
+print(f"my name is {name} I am {age}")
+
+#variables 3
+is_student = True
+print(is_student)
+
+#variables 4
+print(len("Maite"))
+print(len(name))
+
+#variables 5
+name, surname, city = "Maite", "Gómez", "Orihuela"
+print("My name is", name, surname, "I'm from", city)
+
+#variables 6
+color = input("what is your favourite color?")
+print(color)
+
+#variables 7
+fruit = 3
+print(type(fruit))
+fruit = "manzana"
+print(fruit)
+print(type(fruit))
+
+# variables 8
+price = 3.5
+print(int(price))
+price = 3.9
+print(int(price))
+
+#variables 9
+address_len = len("Orihuela in Alicante")
+print(address_len)
+
+#variables 10
+phone: int = 123456789
+print(type(phone))
+phone = 987654321
+print(type(phone))
+phone = "pear"
+print(type(phone))
