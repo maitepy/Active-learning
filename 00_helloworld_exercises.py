3d0c7f0cc1d3db3a050275e9a1ba2b9a2e35e821
maitepy <maite@estudiodesoluciones.com>
2025-02-20 18:56:53 +0100

    Add files via upload

---
+++ 00_helloworld_exercises.py
@@ -0,0 +1,22 @@
+print("Hola Mundo")
+# imprimimos un string que hay dentro del paréntesis
+print("Maite, 43")
+print(type("Maite"))
+print(type(43))
+print(type(3.2))
+"""
+los datos pueden ser cadenas de texto o strains str
+pueden ser numeros enteros o integers int
+o pueden ser decimales o float también 
+también pueden ser números reales aunque aun no vamos a 
+trabajar con ellos
+"""
+print("Hola"+" "+"Mundo")
+name = "Maite"
+age = 43
+print(name, age)
+print("My name is", name, "and I am", age)
+name = input("What is your name?")
+print("Hello", name)
+print(32+54) #imprime el resultado de una suma de dos números enteros
+print(type(32+54)) #imprime el tipo de dato resultante de la suma anterior, o sea int, entero
